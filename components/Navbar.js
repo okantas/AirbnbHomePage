8eb9ed91593511ec5de44b2905c6f51fe18ff9a8
okantas <okantas954@gmail.com>
2024-12-27 22:25:56 +0300

    titles are implemented

--- components/Navbar.js
+++ components/Navbar.js
@@ -17,77 +17,77 @@ export default function navbar() {
         className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
       >
         <Image src={Views} alt="amazing views" />
-        <p className="text-xs my-2">Amazing Views</p>
+        <p className="text-xs my-2">Lakefront</p>
       </a>
       <a
         href="#"
         className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
       >
         <Image src={Views} alt="amazing views" />
-        <p className="text-xs my-2">Amazing Views</p>
+        <p className="text-xs my-2">Caves</p>
       </a>
       <a
         href="#"
         className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
       >
         <Image src={Views} alt="amazing views" />
-        <p className="text-xs my-2">Amazing Views</p>
+        <p className="text-xs my-2">OMG!</p>
       </a>
       <a
         href="#"
         className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
       >
         <Image src={Views} alt="amazing views" />
-        <p className="text-xs my-2">Amazing Views</p>
+        <p className="text-xs my-2">Earth Homes</p>
       </a>
       <a
         href="#"
         className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
       >
         <Image src={Views} alt="amazing views" />
-        <p className="text-xs my-2">Amazing Views</p>
+        <p className="text-xs my-2">Treehouses</p>
       </a>
       <a
         href="#"
         className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
       >
         <Image src={Views} alt="amazing views" />
-        <p className="text-xs my-2">Amazing Views</p>
+        <p className="text-xs my-2">Mansions</p>
       </a>
       <a
         href="#"
         className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
       >
         <Image src={Views} alt="amazing views" />
-        <p className="text-xs my-2">Amazing Views</p>
+        <p className="text-xs my-2">Cabins</p>
       </a>
       <a
         href="#"
         className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
       >
         <Image src={Views} alt="amazing views" />
-        <p className="text-xs my-2">Amazing Views</p>
+        <p className="text-xs my-2">Amazing Pools</p>
       </a>
       <a
         href="#"
         className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
       >
         <Image src={Views} alt="amazing views" />
-        <p className="text-xs my-2">Amazing Views</p>
+        <p className="text-xs my-2">Islands</p>
       </a>
       <a
         href="#"
         className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
       >
         <Image src={Views} alt="amazing views" />
-        <p className="text-xs my-2">Amazing Views</p>
+        <p className="text-xs my-2">Beacfront</p>
       </a>
       <a
         href="#"
         className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
       >
         <Image src={Views} alt="amazing views" />
-        <p className="text-xs my-2">Amazing Views</p>
+        <p className="text-xs my-2">Trending</p>
       </a>
     </nav>
   );
