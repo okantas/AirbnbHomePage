60f364b52c1afd8f96f8ec65df8d0cabe356b224
okantas <okantas954@gmail.com>
2024-12-27 23:18:42 +0300

    conntet component is created

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
+import Content from "@/components/Content";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Navbar />
+      <Content />
     </div>
   );
 }
