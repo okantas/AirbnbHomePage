779e369765efe40a5fe6998ccd9649b0f7735214
okantas <okantas954@gmail.com>
2024-12-27 22:22:38 +0300

    duplication is ready

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,11 +9,84 @@ export default function navbar() {
         href="#"
         className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
       >
-        <Image
-          src={Views}
-          alt="amazing views"
-          className="fill-slate-500 stroke-slate-500"
-        />
+        <Image src={Views} alt="amazing views" />
+        <p className="text-xs my-2">Amazing Views</p>
+      </a>
+      <a
+        href="#"
+        className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
+      >
+        <Image src={Views} alt="amazing views" />
+        <p className="text-xs my-2">Amazing Views</p>
+      </a>
+      <a
+        href="#"
+        className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
+      >
+        <Image src={Views} alt="amazing views" />
+        <p className="text-xs my-2">Amazing Views</p>
+      </a>
+      <a
+        href="#"
+        className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
+      >
+        <Image src={Views} alt="amazing views" />
+        <p className="text-xs my-2">Amazing Views</p>
+      </a>
+      <a
+        href="#"
+        className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
+      >
+        <Image src={Views} alt="amazing views" />
+        <p className="text-xs my-2">Amazing Views</p>
+      </a>
+      <a
+        href="#"
+        className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
+      >
+        <Image src={Views} alt="amazing views" />
+        <p className="text-xs my-2">Amazing Views</p>
+      </a>
+      <a
+        href="#"
+        className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
+      >
+        <Image src={Views} alt="amazing views" />
+        <p className="text-xs my-2">Amazing Views</p>
+      </a>
+      <a
+        href="#"
+        className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
+      >
+        <Image src={Views} alt="amazing views" />
+        <p className="text-xs my-2">Amazing Views</p>
+      </a>
+      <a
+        href="#"
+        className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
+      >
+        <Image src={Views} alt="amazing views" />
+        <p className="text-xs my-2">Amazing Views</p>
+      </a>
+      <a
+        href="#"
+        className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
+      >
+        <Image src={Views} alt="amazing views" />
+        <p className="text-xs my-2">Amazing Views</p>
+      </a>
+      <a
+        href="#"
+        className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
+      >
+        <Image src={Views} alt="amazing views" />
+        <p className="text-xs my-2">Amazing Views</p>
+      </a>
+      <a
+        href="#"
+        className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
+      >
+        <Image src={Views} alt="amazing views" />
         <p className="text-xs my-2">Amazing Views</p>
       </a>
     </nav>
