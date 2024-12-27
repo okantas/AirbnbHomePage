16a23b42be40de4d66607e54365ea1decf155840
okantas <okantas954@gmail.com>
2024-12-27 11:52:55 +0300

    one navbar item is implemented

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Image from "next/image";
+import Views from "../public/amazingviews.svg";
 
 export default function navbar() {
   return (
-    <div>
-      arabam daica
-      <div>dekd</div>
-    </div>
+    <nav className="flex gap-8 items-center justify-center py-9">
+      <div className="flex flex-col justify-center items-center gap-1 ">
+        <Image src={Views} alt="amazing views" className="w-6 h-6" />
+        <a>Amazing Views</a>
+      </div>
+    </nav>
   );
 }
