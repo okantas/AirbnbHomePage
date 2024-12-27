879ab88f1d7b722c05e47c531aa87fd0f3d970d5
okantas <okantas954@gmail.com>
2024-12-27 22:14:17 +0300

    icon's animation and hover is get ready

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,10 +5,17 @@ import Views from "../public/amazingviews.svg";
 export default function navbar() {
   return (
     <nav className="flex gap-8 items-center justify-center py-9">
-      <div className="flex flex-col justify-center items-center gap-1 ">
-        <Image src={Views} alt="amazing views" className="w-6 h-6" />
-        <a>Amazing Views</a>
-      </div>
+      <a
+        href="#"
+        className="flex flex-col  justify-center items-center text-slate-500 px-1 py-3 gap-1 hover:font-bold hover:border-solid hover:border-b-2 hover:border-black hover:text-black transition-all duration-150 delay-100 ease-in-out"
+      >
+        <Image
+          src={Views}
+          alt="amazing views"
+          className="fill-slate-500 stroke-slate-500"
+        />
+        <p className="text-xs my-2">Amazing Views</p>
+      </a>
     </nav>
   );
 }
