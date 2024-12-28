2d2e027bf37bdd3cd6bbc8dcc0e8cb7ea150778f
okantas <okantas954@gmail.com>
2024-12-28 23:28:47 +0300

    first card is ready

--- components/Content.js
+++ components/Content.js
@@ -1,5 +1,17 @@
 import React from "react";
+import Image from "next/image";
+import Airbnb1 from "../public/airbnb1.svg";
 
 export default function Content() {
-  return <div></div>;
+  return (
+    <main>
+      <div className="flex flex-col gap-1 ">
+        <Image src={Airbnb1} alt="First image" className="mb-3" />
+        <h1 className="font-bold text-sm">Knoxville, Tennessee</h1>
+        <p className=" text-sm">Mountain and lake views</p>
+        <p className=" text-sm">Apr 16 - 21</p>
+        <p className="font-bold text-sm">$388 night</p>
+      </div>
+    </main>
+  );
 }
