dcc9dfc6218126395f74e8a700e95bbce3d2d337
okantas <okantas954@gmail.com>
2024-12-29 00:54:52 +0300

    images are updated

--- components/Content.js
+++ components/Content.js
@@ -4,7 +4,106 @@ import Airbnb1 from "../public/airbnb1.svg";
 
 export default function Content() {
   return (
-    <main className="mx-20">
+    <main className="mx-20 grid grid-cols-4 justify-center place-items-center max-w-full gap-6">
+      <div className="flex flex-col gap-1 cursor-pointer ">
+        <Image src={Airbnb1} alt="First image" className="mb-3" />
+        <h1 className="font-bold text-sm">Knoxville, Tennessee</h1>
+        <p className=" text-slate-500 text-sm">Mountain and lake views</p>
+        <p className="text-slate-500 text-sm">Apr 16 - 21</p>
+        <p className="text-sm">
+          <span className="font-bold">$388</span> night
+        </p>
+      </div>
+      <div className="flex flex-col gap-1 cursor-pointer ">
+        <Image src={Airbnb1} alt="First image" className="mb-3" />
+        <h1 className="font-bold text-sm">Knoxville, Tennessee</h1>
+        <p className=" text-slate-500 text-sm">Mountain and lake views</p>
+        <p className="text-slate-500 text-sm">Apr 16 - 21</p>
+        <p className="text-sm">
+          <span className="font-bold">$388</span> night
+        </p>
+      </div>
+      <div className="flex flex-col gap-1 cursor-pointer ">
+        <Image src={Airbnb1} alt="First image" className="mb-3" />
+        <h1 className="font-bold text-sm">Knoxville, Tennessee</h1>
+        <p className=" text-slate-500 text-sm">Mountain and lake views</p>
+        <p className="text-slate-500 text-sm">Apr 16 - 21</p>
+        <p className="text-sm">
+          <span className="font-bold">$388</span> night
+        </p>
+      </div>
+      <div className="flex flex-col gap-1 cursor-pointer ">
+        <Image src={Airbnb1} alt="First image" className="mb-3" />
+        <h1 className="font-bold text-sm">Knoxville, Tennessee</h1>
+        <p className=" text-slate-500 text-sm">Mountain and lake views</p>
+        <p className="text-slate-500 text-sm">Apr 16 - 21</p>
+        <p className="text-sm">
+          <span className="font-bold">$388</span> night
+        </p>
+      </div>
+      <div className="flex flex-col gap-1 cursor-pointer ">
+        <Image src={Airbnb1} alt="First image" className="mb-3" />
+        <h1 className="font-bold text-sm">Knoxville, Tennessee</h1>
+        <p className=" text-slate-500 text-sm">Mountain and lake views</p>
+        <p className="text-slate-500 text-sm">Apr 16 - 21</p>
+        <p className="text-sm">
+          <span className="font-bold">$388</span> night
+        </p>
+      </div>
+      <div className="flex flex-col gap-1 cursor-pointer ">
+        <Image src={Airbnb1} alt="First image" className="mb-3" />
+        <h1 className="font-bold text-sm">Knoxville, Tennessee</h1>
+        <p className=" text-slate-500 text-sm">Mountain and lake views</p>
+        <p className="text-slate-500 text-sm">Apr 16 - 21</p>
+        <p className="text-sm">
+          <span className="font-bold">$388</span> night
+        </p>
+      </div>
+      <div className="flex flex-col gap-1 cursor-pointer ">
+        <Image src={Airbnb1} alt="First image" className="mb-3" />
+        <h1 className="font-bold text-sm">Knoxville, Tennessee</h1>
+        <p className=" text-slate-500 text-sm">Mountain and lake views</p>
+        <p className="text-slate-500 text-sm">Apr 16 - 21</p>
+        <p className="text-sm">
+          <span className="font-bold">$388</span> night
+        </p>
+      </div>
+      <div className="flex flex-col gap-1 cursor-pointer ">
+        <Image src={Airbnb1} alt="First image" className="mb-3" />
+        <h1 className="font-bold text-sm">Knoxville, Tennessee</h1>
+        <p className=" text-slate-500 text-sm">Mountain and lake views</p>
+        <p className="text-slate-500 text-sm">Apr 16 - 21</p>
+        <p className="text-sm">
+          <span className="font-bold">$388</span> night
+        </p>
+      </div>
+      <div className="flex flex-col gap-1 cursor-pointer ">
+        <Image src={Airbnb1} alt="First image" className="mb-3" />
+        <h1 className="font-bold text-sm">Knoxville, Tennessee</h1>
+        <p className=" text-slate-500 text-sm">Mountain and lake views</p>
+        <p className="text-slate-500 text-sm">Apr 16 - 21</p>
+        <p className="text-sm">
+          <span className="font-bold">$388</span> night
+        </p>
+      </div>
+      <div className="flex flex-col gap-1 cursor-pointer ">
+        <Image src={Airbnb1} alt="First image" className="mb-3" />
+        <h1 className="font-bold text-sm">Knoxville, Tennessee</h1>
+        <p className=" text-slate-500 text-sm">Mountain and lake views</p>
+        <p className="text-slate-500 text-sm">Apr 16 - 21</p>
+        <p className="text-sm">
+          <span className="font-bold">$388</span> night
+        </p>
+      </div>
+      <div className="flex flex-col gap-1 cursor-pointer ">
+        <Image src={Airbnb1} alt="First image" className="mb-3" />
+        <h1 className="font-bold text-sm">Knoxville, Tennessee</h1>
+        <p className=" text-slate-500 text-sm">Mountain and lake views</p>
+        <p className="text-slate-500 text-sm">Apr 16 - 21</p>
+        <p className="text-sm">
+          <span className="font-bold">$388</span> night
+        </p>
+      </div>
       <div className="flex flex-col gap-1 cursor-pointer ">
         <Image src={Airbnb1} alt="First image" className="mb-3" />
         <h1 className="font-bold text-sm">Knoxville, Tennessee</h1>
