f31003e784b6f8b5feeac0d1c956cbcd0f8edeb0
okantas <okantas954@gmail.com>
2024-12-29 00:12:57 +0300

    updated in some texts

--- components/Content.js
+++ components/Content.js
@@ -4,13 +4,15 @@ import Airbnb1 from "../public/airbnb1.svg";
 
 export default function Content() {
   return (
-    <main>
-      <div className="flex flex-col gap-1 ">
+    <main className="mx-20">
+      <div className="flex flex-col gap-1 cursor-pointer ">
         <Image src={Airbnb1} alt="First image" className="mb-3" />
         <h1 className="font-bold text-sm">Knoxville, Tennessee</h1>
-        <p className=" text-sm">Mountain and lake views</p>
-        <p className=" text-sm">Apr 16 - 21</p>
-        <p className="font-bold text-sm">$388 night</p>
+        <p className=" text-slate-500 text-sm">Mountain and lake views</p>
+        <p className="text-slate-500 text-sm">Apr 16 - 21</p>
+        <p className="text-sm">
+          <span className="font-bold">$388</span> night
+        </p>
       </div>
     </main>
   );
