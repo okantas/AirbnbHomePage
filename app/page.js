b1a518287c388391b269babc5fdf006b63896792
okantas <okantas954@gmail.com>
2024-12-29 23:36:33 +0300

    search bar is implementing

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Content from "@/components/Content";
+import SearchBar from "@/components/SearchBar";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Navbar />
+      <SearchBar />
       <Content />
     </div>
   );
---
+++ components/SearchBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Form from "next/form";
+
+export default function SearchBar() {
+  return (
+    <>
+      <div className="w-full flex place-items-center justify-center">
+        <div className="flex place-items-center justify-center border-black border-solid border-2 w-fit gap-5 p-3 rounded-2xl mb-5">
+          <h1 className="font-bold text-sm">Display total price</h1>
+          <p className="text-slate-500 ">|</p>
+          <Form action="/search" className="flex gap-20">
+            <input
+              name="query"
+              placeholder="Includes all fees, before taxes "
+              className="w-64"
+            />
+            <button
+              type="submit"
+              className="text-sm border-black border-solid border-2 p-1 rounded-full px-3 hover:bg-pink-500"
+            >
+              submit
+            </button>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
+}
